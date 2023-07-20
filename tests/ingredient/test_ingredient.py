6a47fc0ff735c5155ab18a0ea7e5e184c864802d
Luis <l-serrato@live.com>
2023-07-20 15:14:19 -0300

    req1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,9 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ingredient = Ingredient('onion', 1, 'cup')
-    assert ingredient.name == 'onion'
-    assert ingredient.quantity == 1
-    assert ingredient.unit == 'cup'
+    cheese = Ingredient("queijo mussarela")
+    assert cheese.name == "queijo mussarela"
+    assert cheese.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    cheese_hash = Ingredient("queijo mussarela").__hash__()
+    assert cheese_hash == hash("queijo mussarela")
+
+    cheese_eq = Ingredient("queijo mussarela").__eq__(cheese)
+    assert cheese_eq
+
+    cheese_repr = Ingredient("queijo mussarela").__repr__()
+    assert cheese_repr == "Ingredient('queijo mussarela')"
