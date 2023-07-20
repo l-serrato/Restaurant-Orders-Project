c73121843c520ab34f64a7d0763aa88b917565b5
Luis <l-serrato@live.com>
2023-07-20 14:55:29 -0300

    setup

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,7 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('onion', 1, 'cup')
+    assert ingredient.name == 'onion'
+    assert ingredient.quantity == 1
+    assert ingredient.unit == 'cup'
